c7ed7c534abecab8c1cda7e0bc71feb16fd136f3
Pixel <scott.blatt.0b10@gmail.com>
2025-02-07 19:26:59 -0600

    more disablments

--- overlays/disable-broken-checks/default.nix
+++ overlays/disable-broken-checks/default.nix
@@ -3,6 +3,7 @@
 final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [(py-final: py-prev: {
     pytest-xdist = py-prev.pytest-xdist.overrideAttrs (oldAttrs: {
+      doInstallCheck = false;
       doCheck = false;
     });
   })];

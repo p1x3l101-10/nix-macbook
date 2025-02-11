589f0d4e07c9f9d558eda2324da7a281d3cce500
Pixel <scott.blatt.0b10@gmail.com>
2025-02-11 11:44:10 -0600

    save myself

--- overlays/use-custom-nix/default.nix
+++ overlays/use-custom-nix/default.nix
@@ -3,11 +3,11 @@
 final: prev:
 
 let
-  nixPkg = (import ../../modules/home/base/nix.nix { inherit lib; }).nix.package;
+  nixPkg = (import ../../modules/home/base/nix.nix { inherit lib; pkgs = prev; inputs = {}; }).nix.package;
 in
 
 {
-  nixos-option.override {
+  nixos-option.override = {
     nix = nixPkg;
-  }
+  };
 }

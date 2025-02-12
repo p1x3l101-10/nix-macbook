4cbbf8b9dda8d28505d8f9154a4a149aefa44bcf
Pixel <scott.blatt.0b10@gmail.com>
2025-02-12 15:35:58 -0600

    remove homeManager when using darwin entry

--- modules/home/top-level/default.nix
+++ modules/home/top-level/default.nix
@@ -1,7 +1,9 @@
-{ inputs, ... }:
+{ lib, inputs, ... }:
 
 {
+  # Load all files from homeManager without the HM binary
   imports = with inputs; (with self.homeModules; [
     base
   ]);
+  programs.home-manager.enable = lib.mkForce true;
 }
